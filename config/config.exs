2545b5529301c54b99e3c1d044136936572f7564
Vasile Buza <gmail: vasilebuza07@gmail.com>
2016-07-10 14:56:10 +0300

    My first Phoenix API

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :peepchat,
+  ecto_repos: [Peepchat.Repo]
+
+# Configures the endpoint
+config :peepchat, Peepchat.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "VrB+4DMPbHP3ZyMiGjSrvIgg69hxYuLtea++aLGvSYlyWEhBowcfxMSia5E9gTjv",
+  render_errors: [view: Peepchat.ErrorView, accepts: ~w(json)],
+  pubsub: [name: Peepchat.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+
+config :phoenix, :format_encoders,
+  "json-api": Poison
+
+config :plug, :mimes, %{
+  "application/vnd.api+json" => ["json-api"]
+}
+
+import_config "#{Mix.env}.exs"
